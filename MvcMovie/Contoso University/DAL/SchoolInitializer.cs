24f2895e375448f8fd5b338225d0b6530100d3ea
2686469571 <44010126+2686469571@users.noreply.github.com>
2019-01-09 20:27:11 +0800

    ...

---
+++ MvcMovie/Contoso University/DAL/SchoolInitializer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            //构建学生数据
+            var students = new List<Student>
+            {
+            new Student { Name="E",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            new Student { Name="D",EnrollmentDate=DateTime.Parse("2005-09-03")},
+            new Student { Name="G",EnrollmentDate=DateTime.Parse("2005-09-02")},
+            new Student { Name="叶",EnrollmentDate=DateTime.Parse("2005-09-05")},
+            new Student { Name="问",EnrollmentDate=DateTime.Parse("2005-09-06")},
+        
+            };
+         
+               //将学生数据加入实体类
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+            //构建课程数据
+            var courses = new List<Course>
+            {
+            new Course{CourseID=1050,Title="C#",Credits=3,},
+            new Course{CourseID=4022,Title="Web开发",Credits=3,},
+            new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
+            new Course{CourseID=1045,Title="Calculus",Credits=4,},
+            new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
+            new Course{CourseID=2021,Title="Composition",Credits=3,},
+            new Course{CourseID=2042,Title="Literature",Credits=4,}
+            };  
+            //将课程数据加入实体类
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+
+            //构建注册数据
+      
+            var enrollments = new List<Enrollment>
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=1050,},
+            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+            new Enrollment{StudentID=6,CourseID=1045},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
+            //将构建注册数据加入实体类
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        //但首先需要短所要看起来更美观的发行日期。
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+       
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
